e508727895a20692d527986f56d7245d4b50ea82
anamtorun <anamtorun@gmail.com>
2021-11-14 19:26:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wshhp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+       try {
+             await client.connect();
+             const database = client.db('volt_rider');
+             const bikesCollection = database.collection('bikes');             
+       }
+       finally {
+        //    await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Listening to Port:${port}`)
+})
